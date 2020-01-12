8cb47fc6cab79e3f0b6e10e78e4c3d19f2e864df
Bintang Diyantoro <bintangdiyantoro@gmail.com>
2020-01-12 21:48:08 +0700

    Row & Column, cross & main axis

--- lib/main.dart
+++ lib/main.dart
@@ -14,29 +14,72 @@ class MyApp extends StatelessWidget {
           elevation: 10,
         ),
         body: Center(
-          child: Container(
-            color: Colors.green[200],
             child: Column(
-              children: <Widget>[
-                Text(
-                  "Halo Mas! \nketemu lagi kita :)",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 40,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              color: Colors.green[200],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    "Halo Mas! \nketemu lagi kita :)",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 40,
+                    ),
                   ),
-                ),
-                RaisedButton.icon(
-                  onPressed: () {},
-                  label: Text("Tekan aku mas"),
-                  icon: Icon(Icons.face),
-                )
-              ],
+                  RaisedButton.icon(
+                    onPressed: () {},
+                    label: Text("Tekan aku mas"),
+                    icon: Icon(Icons.face),
+                  )
+                ],
+              ),
+              // padding: EdgeInsets.all(20),
+              height: 230,
+              width: 300,
             ),
-            padding: EdgeInsets.all(20),
-            height: 230,
-            width: 300,
-          ),
-        ),
+            Container(
+              color: Colors.green[200],
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      "1",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    padding: EdgeInsets.all(20),
+                    color: Colors.blueGrey,
+                  ),
+                  Container(
+                    child: Text(
+                      "2",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    padding: EdgeInsets.all(20),
+                    color: Colors.black45,
+                  ),
+                  Container(
+                    child: Text(
+                      "3",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    padding: EdgeInsets.all(20),
+                    color: Colors.deepPurple,
+                  ),
+                ],
+              ),
+              // padding: EdgeInsets.all(20),
+              height: 230,
+              width: 300,
+            ),
+          ],
+        )),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.green[800],
           onPressed: () {},
